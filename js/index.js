1eccc79b981f28f426f72f46af0ae32f57a64536
Utsav Thakur <utsav@bay20.com>
2025-02-05 18:13:26 +0545

    design changes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,42 @@
 $('.owl-carousel').owlCarousel({
-       loop: true,
-       margin: 10,
-       responsiveClass: true,
-       items: 1
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  items: 1
 })
+
 
 
 let header = document.getElementById('header');
+
 document.addEventListener('scroll', () => {
-       if (window.scrollY > 15) {
-              header.classList.add('scroll')
-       }else{
-              header.classList.remove('scroll')
-       }
+  if (window.innerWidth > 1023) {
+    if (window.scrollY > 15) {
+      header.classList.add('scroll');
+    } else {
+      header.classList.remove('scroll');
+    }
+  } else {
+    header.classList.remove('scroll');
+  }
+});
+
+
+let menu = document.getElementById('menu');
+let nav = document.getElementById('nav');
+let cross = document.getElementById('cross');
+menu.addEventListener('click', () => {
+  nav.classList.toggle('active')
 })
-console.log(header)
+cross.addEventListener('click', () => {
+  nav.classList.remove('active')
+})
+
+let footer = document.querySelectorAll('.col');
+
+footer.forEach(item => {
+  item.addEventListener('click', () => {
+
+    item.classList.toggle('toggle');
+  });
+});
